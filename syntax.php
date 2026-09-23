77570729b203a1bd77bd10266d0f9c566f7ed0de
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-09-23 14:31:38 -0300

    Use section APIs for tab level wrappers
    Replace manual level wrapper `<div>` output in `syntax.php` with `section_open($level)`/`section_close()`. This keeps tab content wrapped as a proper section so nested plugins (like include) no longer interfere by prematurely closing tab wrappers.

--- syntax.php
+++ syntax.php
@@ -147,8 +147,8 @@ class syntax_plugin_tabbox extends DokuWiki_Syntax_Plugin {
         $R->doc .= $R->_xmlEntities($name);
         $R->doc .= "</h$level>" . DOKU_LF;
         
-        // start a dummy section level div so nested plugins like 'include' don't close the tabboxtab early
-        $R->doc .= '<div class="level' . $level . '">' . DOKU_LF;
+        // start a section level wrapper so nested plugins like 'include' don't close the tabboxtab early
+        $R->section_open($level);
 
         $this->intab = true;
     }
@@ -161,7 +161,7 @@ class syntax_plugin_tabbox extends DokuWiki_Syntax_Plugin {
      */
     protected function _closeTab(Doku_Renderer_xhtml $R, $pos) {
         $R->finishSectionEdit($pos);
-        $R->doc .= DOKU_LF.'</div>'.DOKU_LF; // close level wrapper
+        $R->section_close(); // close level wrapper
         $R->doc .= DOKU_LF.'</div>'.DOKU_LF; // close tabboxtab
         $this->intab = false;
     }
